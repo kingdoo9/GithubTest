d663c655bd8eab5cf01e598c27a788ea65eca450
강승덕 <skung723@gmail.com>
2021-04-24 12:25:12 +0900

    firstinit

---
+++ GithubTest/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GithubTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("button click");
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("button2 click");
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+        }
+    }
+}
